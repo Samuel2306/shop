869346d38d5dcd16a150cefe502d2f3355feeac2
feng.shen <feng.shen@nio.com>
2020-07-09 16:39:55 +0800

    添加server文件

---
+++ pages/user/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    这是用户页面
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
